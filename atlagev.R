c37d0cc84be5964437835a92137b7cafd4bbdbca
Kalicz Péter <kaliczp@gmail.com>
2024-04-02 15:32:23 +0200

    Print the rounded values

--- atlagev.R
+++ atlagev.R
@@ -3,7 +3,11 @@ for(honap in 1:12){
  homatlag[honap] <- mean(as.vector(coredata(hom.xts[seq(from = honap, by = 12, length = 30)])))
 }
 
+round(homatlag,2)
+
 csapatlag <- numeric(12)
 for(honap in 1:12){
  csapatlag[honap] <- mean(as.vector(coredata(csap.xts[seq(from = honap, by = 12, length = 30)])))
 }
+
+round(csapatlag,2)
