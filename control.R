6571d6f54542245fd0f4d4b9d5c3dad02e39c7ac
Kalicz Péter <kaliczp@gmail.com>
2024-04-04 09:41:14 +0200

    Different names for control

--- control.R
+++ control.R
@@ -1,11 +1,18 @@
 ## Az első csv adataival a havira.R szkriptből
-hom.xts <- apply.monthly(homcsap193060.xts["1989/2019","ta"], mean)
-csap.xts <- apply.monthly(homcsap193060.xts["1989/2019","rr"], sum)
+homcntr.xts <- apply.monthly(homcsap193060.xts["1989/2019","ta"], mean)
+csapcntr.xts <- apply.monthly(homcsap193060.xts["1989/2019","rr"], sum)
 
-csapatlag <- numeric(12)
+csapatlagcntr <- numeric(12)
 for(honap in 1:12){
- csapatlag[honap] <- mean(as.vector(coredata(csap.xts[seq(from = honap, by = 12, length = 30)])))
+ csapatlagcntr[honap] <- mean(as.vector(coredata(csapcntr.xts[seq(from = honap, by = 12, length = 30)])))
 }
 
-round(csapatlag,2)
-sum(csapatlag)
+round(csapatlagcntr,2)
+sum(csapatlagcntr)
+
+idocntr <- seq(as.Date("2022-01-15"),as.Date("2022-12-15"), by = "month")
+
+csapatlag.xts <- xts(csapatlag, idocntr)
+csapatlagcntr.xts <- xts(csapatlagcntr, idocntr)
+
+csapatlag.xts - csapatlagcntr.xts
