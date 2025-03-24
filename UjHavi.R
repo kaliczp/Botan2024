00adf792f438c3ee561d2f5a037f9363a07b42c3
Kalicz Péter <kaliczp@gmail.com>
2025-03-24 09:30:41 +0100

    Mean is rouded to 2 decimals

--- UjHavi.R
+++ UjHavi.R
@@ -8,7 +8,7 @@ Botanxts <- xts(Botanraw[,-1], as.Date(Botanraw[,1]))
 ### Monthly
 MinTempB <- apply.monthly(Botanxts[,1], min)
 MaxTempB <- apply.monthly(Botanxts[,2], max)
-MeanTempB <- apply.monthly(Botanxts[,3], colMeans)
+MeanTempB <- apply.monthly(Botanxts[,3], function(x){round(colMeans(x),2)})
 SumCsapB <- apply.monthly(Botanxts[,4], function(x){sum(x, na.rm = TRUE)})
 ## Glue data
 BotanMonth <- cbind(MinT = MinTempB, MaxT = MaxTempB, MeanT = MeanTempB, SumP = SumCsapB)
