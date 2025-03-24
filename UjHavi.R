f0fe227d3271126247fa544946453ac69a5cf6d2
Kalicz Péter <kaliczp@gmail.com>
2025-03-24 09:17:39 +0100

    Calculate monthly data for each column

--- UjHavi.R
+++ UjHavi.R
@@ -5,3 +5,8 @@ Botanraw <- as.data.frame(read_excel("Botanikus kert 1980-2019.xlsx"))[,c(1:4,8)
 library(xts)
 ## Create data matrix without first column and use Date from first column
 Botanxts <- xts(Botanraw[,-1], as.Date(Botanraw[,1]))
+### Monthly
+MinTempB <- apply.monthly(Botanxts[,1], min)
+MaxTempB <- apply.monthly(Botanxts[,2], max)
+MeanTempB <- apply.monthly(Botanxts[,3], colMeans)
+SumCsapB <- apply.monthly(Botanxts[,4], sum)
