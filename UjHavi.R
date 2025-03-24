92b4fcdf52aeb0010bd527e5ea065fa93427f1b2
Kalicz Péter <kaliczp@gmail.com>
2025-03-24 09:10:28 +0100

    Convert data to xts

--- UjHavi.R
+++ UjHavi.R
@@ -1,3 +1,7 @@
 library(readxl)
 ## Import from excel
 Botanraw <- as.data.frame(read_excel("Botanikus kert 1980-2019.xlsx"))[,c(1:4,8)]
+### Convert to xts
+library(xts)
+## Create data matrix without first column and use Date from first column
+Botanxts <- xts(Botanraw[,-1], as.Date(Botanraw[,1]))
