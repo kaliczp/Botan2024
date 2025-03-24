7a5199eb75b399ac5fec2e0b2c6d34fe20f7493e
Kalicz Péter <kaliczp@gmail.com>
2025-03-24 09:24:00 +0100

    Calculate precipitation sum if cell is empty

--- UjHavi.R
+++ UjHavi.R
@@ -9,6 +9,6 @@ Botanxts <- xts(Botanraw[,-1], as.Date(Botanraw[,1]))
 MinTempB <- apply.monthly(Botanxts[,1], min)
 MaxTempB <- apply.monthly(Botanxts[,2], max)
 MeanTempB <- apply.monthly(Botanxts[,3], colMeans)
-SumCsapB <- apply.monthly(Botanxts[,4], sum)
+SumCsapB <- apply.monthly(Botanxts[,4], function(x){sum(x, na.rm = TRUE)})
 ## Glue data
 BotanMonth <- cbind(MinT = MinTempB, MaxT = MaxTempB, MeanT = MeanTempB, SumP = SumCsapB)
